a80a8a08e1dbb2a7288723eed52546ec4f647629
berlingott <simsimondu@gmail.com>
2022-03-11 19:12:07 -0500

    fix startactivityforresult

--- app/src/main/java/uqac/dim/travail_bloc_d/MafagactivityActivity.java
+++ app/src/main/java/uqac/dim/travail_bloc_d/MafagactivityActivity.java
@@ -2,6 +2,8 @@ package uqac.dim.travail_bloc_d;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,22 +14,56 @@ public class MafagactivityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mafag_activity);
+
     }
 
     public void facebookSelected(View view) {
+        Intent data = new Intent();
+        data.putExtra("marque","facebook");
+        data.putExtra("website","https://facebook.com");
 
+        // data.putExtra("site","https://apple.ca");
+        setResult(RESULT_OK,data);
         finish();
     }
     public void windowsSelected(View view) {
+        Intent data = new Intent();
+        data.putExtra("marque","microsoft");
+        data.putExtra("website","https://www.microsoft.com");
+
+        // data.putExtra("site","https://apple.ca");
+        setResult(RESULT_OK,data);
         finish();
+
     }
     public void appleSelected(View view) {
+        Intent data = new Intent();
+        data.putExtra("marque","apple");
+        data.putExtra("website","https://www.apple.com");
+
+        // data.putExtra("site","https://apple.ca");
+        setResult(RESULT_OK,data);
         finish();
+
     }
     public void amazonSelected(View view) {
+        Intent data = new Intent();
+        data.putExtra("marque","amazon");
+        data.putExtra("website","https://www.amazon.com");
+
+        // data.putExtra("site","https://apple.ca");
+        setResult(RESULT_OK,data);
         finish();
+
     }
     public void googleSelected(View view) {
+        Intent data = new Intent();
+        data.putExtra("marque","google");
+        data.putExtra("website","https://www.google.com");
+
+        // data.putExtra("site","https://apple.ca");
+        setResult(RESULT_OK,data);
         finish();
+
     }
 }
--- app/src/main/java/uqac/dim/travail_bloc_d/MainActivity.java
+++ app/src/main/java/uqac/dim/travail_bloc_d/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    static final int CODEMARQUE = 1; //REQUEST CODE
 
 
 
@@ -13,21 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textViewSite = (TextView) findViewById(R.id.textViewSite);
-        TextView textViewMarque = (TextView) findViewById(R.id.textViewMarque);
+        TextView textViewSite = findViewById(R.id.textViewSite);
+        TextView textViewMarque = findViewById(R.id.textViewMarque);
         textViewMarque.setText("Veuillez faire un choix.");
         textViewSite.setText("Veuillez faire un choix.");
     }
 
 
     public void mafagactivity(View view) {
-        String marque = null, website = null;
 
         Intent intent = new Intent(this, MafagactivityActivity.class);
+        startActivityForResult(intent,CODEMARQUE);
 
+    }
 
-
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODEMARQUE) {
+            if (resultCode == RESULT_OK) {
+                TextView textViewMarque = findViewById(R.id.textViewMarque);
+                textViewMarque.setText(data.getStringExtra("marque"));
+                TextView textViewSite = findViewById(R.id.textViewSite);
+                textViewSite.setText(data.getStringExtra("website"));
+            }
+        }
     }
 
 }
